e0821ce87722231a22096e05ccaac1d751afddb1
antonsadovskiy <antonsadovskiy6@gmail.com>
2024-06-14 15:33:14 +0300

    module 3: data types

---
+++ JS_Core_Advanced/data_types/division_without_remainder.js
@@ -0,0 +1,6 @@
+const isFirstDivisibleByTheSecondWithoutARemainder = (first, second) => {
+    return first % second === 0
+}
+
+console.log(isFirstDivisibleByTheSecondWithoutARemainder(6, 3))
+console.log(isFirstDivisibleByTheSecondWithoutARemainder(5, 3))
