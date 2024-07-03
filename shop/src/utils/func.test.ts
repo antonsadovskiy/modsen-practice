377bbf985765080cbe5633f4bc1e1da0d0e19b28
antonsadovskiy <antonsadovskiy6@gmail.com>
2024-07-03 13:30:30 +0300

    first unit test

---
+++ shop/src/utils/func.test.ts
@@ -0,0 +1,52 @@
+import { describe, expect } from "@jest/globals";
+
+import { ProductType } from "@/api/types";
+import { findMinAndMaxPrice } from "@/utils/findMinAndMaxPrice";
+
+describe("test func", () => {
+  test("should find min and max values in array", () => {
+    const data: ProductType[] = [
+      {
+        id: 1,
+        price: 54,
+        description: "",
+        image: "",
+        category: "",
+        rating: { rate: 0, count: 0 },
+        title: "",
+      },
+      {
+        id: 2,
+        price: 2,
+        description: "",
+        image: "",
+        category: "",
+        rating: { rate: 0, count: 0 },
+        title: "",
+      },
+      {
+        id: 3,
+        price: 43,
+        description: "",
+        image: "",
+        category: "",
+        rating: { rate: 0, count: 0 },
+        title: "",
+      },
+      {
+        id: 4,
+        price: 10,
+        description: "",
+        image: "",
+        category: "",
+        rating: { rate: 0, count: 0 },
+        title: "",
+      },
+    ];
+
+    const [min, max] = findMinAndMaxPrice(data);
+
+    expect(min).toBe(2);
+    expect(max).toBe(54);
+  });
+});
