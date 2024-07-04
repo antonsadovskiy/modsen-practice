2cdcab2f0ef13f5299360dc00ac4eedba4f6ca58
antonsadovskiy <antonsadovskiy6@gmail.com>
2024-07-04 16:20:02 +0300

    remove code wars folder

--- codewars/decode_the_morse_code.js
+++
@@ -1,5 +0,0 @@
-
-const decodeMorseCode = (morseCode) => morseCode.split('   ').map(word => word.split(' ').map(character => MORSE_CODE[character]).join('')).join(' ').trim()
-
-
-console.log(decodeMorseCode(".... . -.--   .--- ..- -.. ."))
--- codewars/josephus_survivor.js
+++
@@ -1,9 +0,0 @@
-function josephusSurvivor(n, k) {
-    let survivor = 0;
-    for (let i = 1; i <= n; i++) {
-        survivor = (survivor + k) % i;
-    }
-    return survivor + 1;
-}
-
-console.log(josephusSurvivor(14,2))
